66da60b002326e99e2e55d2c09412cf131cd8046
Eric De Witte <edewitte@vmware.com>
2020-02-26 11:51:19 +0100

    lets get started

---
+++ 0_vmw-cli.sh
@@ -0,0 +1,15 @@
+# source define_download_version_env
+source define_download_version_env
+
+#checking and creating BITSDIR if needed
+if [[ ! -e $BITSDIR ]]; then
+    mkdir $BITSDIR
+fi
+
+sudo apt-get update ; sudo apt-get upgrade
+sudo apt-get install -y build-essential zlibc zlib1g-dev ruby ruby-dev openssl libxslt1-dev libxml2-dev libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 sshpass jq dnsmasq iperf3 sshpass npm
+
+# vwm-cli - requires nodejs >=8
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo apt-get install -y nodejs
+npm install vmw-cli --global
