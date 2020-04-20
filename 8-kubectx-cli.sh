10d899f37331c83e3e7e3e14e45ec6c1a3be0891
Eric De Witte <edewitte@vmware.com>
2020-04-20 13:19:52 +0200

    pks cli

--- 8-kubectx-cli.sh
+++ 8-kubectx-cli.sh
@@ -1,2 +1,15 @@
-kubectl krew install ctx
-kubectl krew install ns
+# source define_download_version_env
+source define_download_version_env
+
+
+#kubectl krew install ctx
+#kubectl krew install ns
+
+curl -L0 https://raw.githubusercontent.com/ahmetb/kubectx/master/kubectx > kubectx
+chmod +x kubectx
+sudo mv kubectx ${BINDIR}/kubectx
+
+
+curl -L0 https://raw.githubusercontent.com/ahmetb/kubectx/master/kubens > kubens
+chmod +x kubens
+sudo mv kubens ${BINDIR}/kubens
